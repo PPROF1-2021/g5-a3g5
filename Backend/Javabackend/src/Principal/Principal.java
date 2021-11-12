e005649d2186cc3342eb6bf3957020a50a6cb21d
Mario <mario.gonzalez@hotmail.com.ar>
2021-11-12 16:43:42 -0300

    Sql

--- Backend/Javabackend/src/Principal/Principal.java
+++ Backend/Javabackend/src/Principal/Principal.java
@@ -1,14 +1,10 @@
 package Principal;
-import java.sql.*;
-import Conector.Conectar_bd;
+//import java.sql.*;
+//import Conector.Conectar_bd;
 
 
 public class Principal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-            Conectar_bd cn=new Conectar_bd();
-            Statement st;
-            ResultSet rs;
-            
     }
 }
